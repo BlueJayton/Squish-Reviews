938ca12dc3b49ab1b4ac47d2f559dacf98cdd702
root <root@DESKTOP-Jayton.localdomain>
2023-03-20 23:14:07 -0500

    All movie info and wiki link done. Displayed on unformatted webpage

--- MovieSite.py
+++ MovieSite.py
@@ -6,38 +6,69 @@ import random
 
 load_dotenv(find_dotenv())
 
+app = flask.Flask(__name__)
+
 list_of_movies = ["Scott%20Pilgrim%20vs.%20the%20World", "Pan%27s%20Labyrinth", "The%20Departed"]
 
 #Make url vars for TMDB API
 TMDB_BASE_URL = "https://api.themoviedb.org/3"
-TMDB_PATH = "/search/movie?api_key="
+TMDB_SEARCH_PATH = "/search/movie?api_key="
+TMDB_GENRE_PATH = "/genre/movie/list?api_key="
+IMG_BASE_URL = "https://image.tmdb.org/t/p/original/"
 
 #Make url vars for MediaWiki API
+WIKI_BASE_URL = "https://en.wikipedia.org/w/api.php?"
+WIKI_URL_PARAMS = "action=query&format=json&prop=info&generator=allpages&inprop=url&gapfrom="
 
-
-#app = flask.Flask(__name__)
-
-#@app.route('/')
+@app.route('/')
 def driver():
     movie = random.choice(list_of_movies)
     TMDB_API_KEY = os.getenv("TMDB_API_KEY")
-    TMDB_QUERY = "&language=en-US&query=" + movie + "&page=1&include_adult=true"
+    TMDB_QUERY = "&language=en-US&query=" + movie + "&page=1&include_adult=false"
     
-    response = requests.get(
-        TMDB_BASE_URL + TMDB_PATH + TMDB_API_KEY + TMDB_QUERY
+    TMDB_genre_response = requests.get(
+        TMDB_BASE_URL + TMDB_GENRE_PATH + TMDB_API_KEY + "&language=en-US"
     )
     
-    movie_info = response.json()["results"]
+    genres_response = TMDB_genre_response.json()["genres"]
     
-    title = movie_info[0]["original_title"]
+    TMDB_response = requests.get(
+        TMDB_BASE_URL + TMDB_SEARCH_PATH + TMDB_API_KEY + TMDB_QUERY
+    )
+    
+    wiki_link_response = requests.get (
+        WIKI_BASE_URL + WIKI_URL_PARAMS + movie + "&gaplimit=5"
+    )
+    
+    movie_info = TMDB_response.json()["results"]
+    
+    #tmdb info
+    title = movie_info[0]["title"]
     description = movie_info[0]["overview"]
+    poster_link = IMG_BASE_URL + movie_info[0]["poster_path"]
     
-    print(title)
-    print(description)
+    #genre info
+    list_of_genre_ids = movie_info[0]["genre_ids"]
+    genre_list = []
+    for index in range(len(genres_response)):
+        for genre_id in list_of_genre_ids:
+            if genres_response[index]["id"] == genre_id:
+                genre_list.append(genres_response[index]["name"])
+    genre_string = ', ' .join(genre_list)
+        
+    #wiki info
+    wiki_page_id = list(wiki_link_response.json()["query"]["pages"]) [0]
+    wiki_link = wiki_link_response.json()["query"]["pages"][wiki_page_id]["fullurl"]
     
-    #return flask.render_template (
-        #"webpage.html"
-    #)
     
-#app.run(debug = True)
-driver()
+
+    return flask.render_template (
+        "webpage.html",
+        title = title,
+        genre_string = genre_string,
+        description = description,
+        poster_link = poster_link,
+        wiki_link = wiki_link
+    )
+    
+app.run(debug = True)
