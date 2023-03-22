c53508464652113441739257e4896a4ca15b542a
root <root@DESKTOP-Jayton.localdomain>
2023-03-22 18:18:41 -0500

    Website fully functional. Now to make it less ugly

---
+++ squish_ratings_driver.py
@@ -0,0 +1,61 @@
+from flask import *
+from dotenv import load_dotenv, find_dotenv
+import random
+import squish_ratings_functions
+
+load_dotenv(find_dotenv())
+
+app = Flask(__name__)
+
+list_of_movies = ["Scott Pilgrim vs. the World", "Pan's Labyrinth", "The Departed", "The Shawshank Redemption", "Django Unchained", 
+                  "Spider-Man: Into the Spider-Verse", "Inglorious Basterds", "Mad Max: Fury Road", "Troll 2", "Hot Fuzz"]
+
+#Make url dict for TMDB API
+TMDB_URL_DICT = {"TMDB_BASE_URL" : "https://api.themoviedb.org/3",
+                 "TMDB_SEARCH_PATH" : "/search/movie?api_key=",
+                 "TMDB_GENRE_PATH" : "/genre/movie/list?api_key=",}
+
+IMG_BASE_URL = "https://image.tmdb.org/t/p/original/"
+
+#MediaWiki API URL dict
+WIKI_URL_DICT = {"WIKI_BASE_URL" : "https://en.wikipedia.org/w/api.php?",
+                 "WIKI_URL_PARAMS" : "action=query&format=json&prop=info&generator=allpages&inprop=url&gapfrom="}
+
+@app.route('/', methods = ['POST', 'GET'])
+def random_movie():
+       
+    movie_title = random.choice(list_of_movies)
+    movie_title_url = squish_ratings_functions.make_url_title(movie_title)
+    movie_info = squish_ratings_functions.get_movie_info_TMDB(movie_title_url, TMDB_URL_DICT, WIKI_URL_DICT, IMG_BASE_URL)
+        
+    if 'Random Movie' in request.form.values():
+        return redirect(url_for("random_movie"))
+    elif 'Search' in request.form.values():
+        movie_title = request.form.get("search_text")
+        movie_title_url = squish_ratings_functions.make_url_title(movie_title)
+        return redirect(url_for("search", movie_title_url = movie_title_url))
+        
+    
+    return render_template (
+        "randommovie.html",
+        movie_info = movie_info,
+    )
+    
+@app.route('/search/<movie_title_url>', methods = ['POST', 'GET'])
+def search(movie_title_url):
+    
+    movie_info = squish_ratings_functions.get_movie_info_TMDB(movie_title_url, TMDB_URL_DICT, WIKI_URL_DICT, IMG_BASE_URL)
+    
+    if 'Random Movie' in request.form.values():
+        return redirect(url_for("random_movie"))
+    elif 'Search' in request.form.values():
+        movie_title = request.form.get("search_text")
+        movie_title_url = squish_ratings_functions.make_url_title(movie_title)
+        return redirect(url_for("search", movie_title_url = movie_title_url)) 
+       
+    return render_template (
+        "searchmovie.html",
+        movie_info = movie_info
+    )    
+    
+app.run(debug = True)
---
+++ squish_ratings_functions.py
@@ -0,0 +1,61 @@
+import os
+import requests
+from urllib import parse
+
+def make_url_title (movie_title):
+    return parse.quote(movie_title)
+
+def get_movie_info_TMDB (movie, TMDB_URL_DICT, WIKI_URL_DICT, IMG_BASE_URL):
+    
+    movie_info = {"title" : "", "description": "",
+                  "poster_link" : "", "poster_height" : "",
+                  "rating" : "", "genre_string" : "",
+                  "wiki_link" : ""}
+    
+    TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+    TMDB_QUERY = "&language=en-US&query=" + movie + "&page=1&include_adult=false"
+    
+    TMDB_genre_response = requests.get(
+        TMDB_URL_DICT['TMDB_BASE_URL'] + TMDB_URL_DICT['TMDB_GENRE_PATH'] + TMDB_API_KEY + "&language=en-US"
+    )
+    
+    genres_response = TMDB_genre_response.json()["genres"]
+    
+    TMDB_response = requests.get(
+        TMDB_URL_DICT['TMDB_BASE_URL'] + TMDB_URL_DICT['TMDB_SEARCH_PATH'] + TMDB_API_KEY + TMDB_QUERY
+    )
+    
+    movie_request_json = TMDB_response.json()["results"]
+    
+    #tmdb info
+    movie_info["title"] = movie_request_json[0]["title"]
+    movie_info["description"] = movie_request_json[0]["overview"]
+    movie_info["poster_link"] = IMG_BASE_URL + movie_request_json[0]["poster_path"]
+    #rating times .1 to make a percentage from 0 to 1
+    movie_info["rating"] = round(movie_request_json[0]["vote_average"], 1)
+    rating_from_0_to_1 = movie_info["rating"] *.1
+    height_calc = 750 * rating_from_0_to_1
+    movie_info["poster_height"] = str(height_calc) + "px"
+    
+    #genre info
+    list_of_genre_ids = movie_request_json[0]["genre_ids"]
+    genre_list = []
+    for index in range(len(genres_response)):
+        for genre_id in list_of_genre_ids:
+            if genres_response[index]["id"] == genre_id:
+                genre_list.append(genres_response[index]["name"])
+    movie_info["genre_string"] = ', ' .join(genre_list)
+    
+    movie_info["wiki_link"] = get_wiki_link(movie, WIKI_URL_DICT)
+    
+    return movie_info
+
+def get_wiki_link (movie, WIKI_URL_DICT):
+    
+    wiki_link_response = requests.get (
+        WIKI_URL_DICT['WIKI_BASE_URL'] + WIKI_URL_DICT['WIKI_URL_PARAMS'] + movie + "&gaplimit=5"
+    )
+    
+    wiki_page_id = list(wiki_link_response.json()["query"]["pages"]) [0]
+    
+    return wiki_link_response.json()["query"]["pages"][wiki_page_id]["fullurl"]
