512372b3c534eccebac352edc96c44f2bb22d287
root <root@DESKTOP-Jayton.localdomain>
2023-04-06 01:06:27 -0500

    added user login and creation and it works with the postgresdb

--- squish-reviews.py
+++ squish-reviews.py
@@ -1,6 +1,8 @@
 from flask import *
 import os
 from dotenv import load_dotenv, find_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
 import random
 import squish_reviews_functions
 
@@ -8,6 +10,36 @@ load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(id):
+    return Person.query.get(int(id))
+
+#DATABASE MODELS
+class Person(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    
+    def is_active(self):
+        return True
+    
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    movieid = db.Column(db.Integer, nullable=False)
+    username = db.Column(db.String(80), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.String(1000), nullable=False)
+
+with app.app_context():
+    db.create_all()
+    
+    
 
 list_of_movies = ["Scott Pilgrim vs. the World", "Pan's Labyrinth", "The Departed", "The Shawshank Redemption", "Django Unchained", 
                   "Spider-Man: Into the Spider-Verse", "Inglorious Basterds", "Mad Max: Fury Road", "Troll 2", "Hot Fuzz"]
@@ -24,6 +56,54 @@ WIKI_URL_DICT = {"WIKI_BASE_URL" : "https://en.wikipedia.org/w/api.php?",
                  "WIKI_URL_PARAMS" : "action=query&format=json&prop=info&generator=allpages&inprop=url&gapfrom="}
 
 @app.route('/', methods = ['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        if "Login" in request.form.values():
+            username = request.form.get("username")
+            return redirect(url_for("login", username=username))
+    
+        if "Register" in request.form.values():
+            username = request.form.get("username")
+            return redirect(url_for("register", username=username))
+    
+    return render_template (
+        "home.html"
+    )
+
+@app.route('/login')
+def login():
+    username = request.args.get('username')
+    user = Person.query.filter_by(username=username).first()
+    
+    if user:
+        login_user(user)
+        return redirect(url_for('random_movie'))
+    else:
+        flash("No user found.")
+        return redirect(url_for("home"))
+    
+
+@app.route('/register')
+def register():
+    username = request.args.get('username')
+    user = Person.query.filter_by(username=username).first()
+    
+    if user:
+        flash("User already exists.")
+        return redirect(url_for('home'))
+    elif user is None:
+        user = Person(username=username)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return redirect(url_for("random_movie"))
+    
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+@app.route('/random', methods = ['POST', 'GET'])
 def random_movie():
        
     movie_title = random.choice(list_of_movies)
@@ -39,13 +119,15 @@ def random_movie():
             "randommovie.html",
             movie_info = movie_info
         )
-        
+    
+
     if "Random Movie: Dev's Favorites" in request.form.values():
         return redirect(url_for("random_movie"))
     elif 'Search' in request.form.values():
         movie_title = request.form.get("search_text")
         movie_title_url = squish_reviews_functions.make_url_title(movie_title)
         return redirect(url_for("search", movie_title_url = movie_title_url))
+    
         
     
     return render_template (
@@ -67,7 +149,8 @@ def search(movie_title_url):
     elif 'Search' in request.form.values():
         movie_title = request.form.get("search_text")
         movie_title_url = squish_reviews_functions.make_url_title(movie_title)
-        return redirect(url_for("search", movie_title_url = movie_title_url)) 
+        return redirect(url_for("search", movie_title_url = movie_title_url))
+    
        
     return render_template (
         "searchmovie.html",
