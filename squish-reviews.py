4dc403b73b7e5c1abfe253288725fa41dfcb2f87
root <root@DESKTOP-Jayton.localdomain>
2023-04-06 05:17:33 -0500

    deployed. changed some requirements

--- squish-reviews.py
+++ squish-reviews.py
@@ -1,6 +1,7 @@
 from flask import *
 import os
 from dotenv import load_dotenv, find_dotenv
+import psycopg2
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
 import random
